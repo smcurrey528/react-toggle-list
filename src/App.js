ac6d2731968e2af81ae4decc8b89bb49b7e385df
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2018-10-09 22:06:13 -0400

    List set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-        <ToggleList />
+       <h1>Toggle List</h1>
     );
   }
 }
