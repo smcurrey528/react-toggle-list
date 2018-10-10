40dd7eee9b4a5286c7d5849bfbd321b02560118c
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2018-10-09 21:42:42 -0400

    project set up

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+// Sample data
+const data = [
+	{
+		id: 1,
+		first: "Judy",
+		last: "Cole",
+		age: 25,
+		state: "Dundee",
+		email: "judy.cole@example.com" 
+	},
+		{
+		id: 2,
+		first: "Gabe",
+		last: "Jimenez",
+		age: 42,
+		state: "Worcestershire",
+		email: "gabe.jimenez@example.com" 
+	},
+		{
+		id: 3,
+		first: "Marc",
+		last: "Ibañez",
+		age: 43,
+		state: "Ceuta",
+		email: "marc.ibañez@example.com" 
+	},
+		{
+		id: 4,
+		first: "Luka",
+		last: "Sievers",
+		age: 49,
+		state: "Bayern",
+		email: "luka.sievers@example.com"
+	}
+]
+
+export default data;
